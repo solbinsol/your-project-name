e9571a752186bc06fe02136fb4fd952a7f4dbe9b
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-31 16:12:17 +0900

    test

--- next.config.js
+++ next.config.js
@@ -4,4 +4,24 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+
+
+module.exports = {
+  pageExtensions: ['jsx', 'js'], // 이 옵션을 추가하여 JS 파일도 페이지로 인식하도록 설정
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://192.168.0.98:4000/api/:path*',
+      }
+    ];
+  },
+
+  // 기본적인  PWA 설정   //나중에 true 로 바꿔야함
+  pwa: { 
+    dest: 'public',
+    register: false,
+    skipWaiting: true,
+  },
+};
