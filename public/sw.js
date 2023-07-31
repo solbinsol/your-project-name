c41d3edad231a38dc5708dcd14f9a51dd8358fa8
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-31 18:31:12 +0900

    sw test1234

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+var title = 'Simple Title';
+var options = {
+  body: 'Simple piece of body text.\nSecond line of body text :)'
+};
+registration.showNotification(title, options);
+
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('my-cache').then((cache) => {
@@ -8,7 +15,16 @@ self.addEventListener('install', (event) => {
       })
     );
   });
+  self.addEventListener('push', event => {
+    const options = {
+      body: 'PWA TEST~~~!', // 푸시 알림에 표시될 메시지
+      icon: '/icons/apple-touch-icon-144x144.png', // 알림에 표시될 아이콘 이미지
+    };
   
+    event.waitUntil(
+      self.registration.showNotification('PWA Test Notification', options)
+    );
+  });
   
   self.addEventListener("fetch", (event) => {
     event.respondWith(
@@ -24,17 +40,5 @@ self.addEventListener('install', (event) => {
         // If both the cache and network fail, return a fallback response here
         return new Response("Fallback response content here", { headers: { "Content-Type": "text/plain" } });
       })
-    );
-  });
-
-
-  self.addEventListener('push', event => {
-    const options = {
-      body: 'PWA TEST', // 푸시 알림에 표시될 메시지
-      icon: '/public/icons/favicon-196x196.png', // 알림에 표시될 아이콘 이미지
-    };
-  
-    event.waitUntil(
-      self.registration.showNotification('PWA Test Notification', options)
     );
   });
